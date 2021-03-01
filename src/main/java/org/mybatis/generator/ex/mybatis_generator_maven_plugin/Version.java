3d65c0ed5df6a3b4f8ca237848a4f65efab75446
rtry <347919582@qq.com>
2021-03-01 14:46:58 +0800

    调整版本号

--- src/main/java/org/mybatis/generator/ex/mybatis_generator_maven_plugin/Version.java
+++ src/main/java/org/mybatis/generator/ex/mybatis_generator_maven_plugin/Version.java
@@ -9,7 +9,7 @@ public final class Version {
     public final static int MinorVersion = 0;
 
     //小版本
-    public final static int RevisionVersion = 4;
+    public final static int RevisionVersion = 5;
 
     public static String getVersionNumber() {
         return Version.MajorVersion + "." + Version.MinorVersion + "." + Version.RevisionVersion;
